8af7fc7299f4e40493eec69316c65461788918c1
Kaan Ceylan <70446695+kaancceylan@users.noreply.github.com>
2022-03-01 17:15:23 +0300

    Add files via upload

--- day-39-apis/flight_data.py
+++ day-39-apis/flight_data.py
@@ -1,7 +1,7 @@
 class FlightData():
 
     def __init__(self, price, origin_city, origin_airport, 
-                dest_city, dest_airport, to_date, return_date):
+                dest_city, dest_airport, to_date, return_date, max_stopovers=0, via_city=""):
             self.price = price
             self.origin_city = origin_city
             self.origin_airport = origin_airport
@@ -9,3 +9,5 @@ class FlightData():
             self.dest_airport = dest_airport
             self.to_date = to_date
             self.return_date = return_date
+            self.max_stopovers = max_stopovers
+            self.via_city = via_city
