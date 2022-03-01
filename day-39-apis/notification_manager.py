ba709ad03bb23488fdd065ff884619c3f84c8a87
Kaan Ceylan <70446695+kaancceylan@users.noreply.github.com>
2022-03-01 17:16:20 +0300

    Add files via upload

--- day-39-apis/notification_manager.py
+++ day-39-apis/notification_manager.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 TWILIO_ACCOUNT_SID = 'ACc96f2e19587a4fc3c3717681bba7d92c'
-TWILIO_AUTH_TOKEN = 'd45b585cc43e1e2f3f28c9648e096c16'
-TWILIO_NUMBER = '++19125518501'
+TWILIO_AUTH_TOKEN = '0909acc1afe90d383d2ab90adede6083'
+TWILIO_NUMBER = '+19125518501'
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
@@ -11,9 +11,9 @@ class NotificationManager:
         self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
     def send_notification(self, message):
-        message = self.client.messages \
-            .create(
+        message = self.client.messages.create(
                 body=message,
                 from_=TWILIO_NUMBER,
                 to='+905306688124'
             )
+        print(message.sid)
